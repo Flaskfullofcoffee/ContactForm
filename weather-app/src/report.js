35e2e289061939c7b04035a5f7b5de2b2ae6fae1
Zac <zdobbs3@gmail.com>
2019-08-05 18:11:47 -0700

    add all the things

--- weather-app/src/report.js
+++ weather-app/src/report.js
@@ -4,7 +4,8 @@ import './index.css';
 const Report = ({ week }) => {
   const weeklyReport = week.map(status => {
     return (
-      <div className="week {status.forecast}" key={status.id}>
+      <div className="week" id={status.forecast} key={status.id}>
+         {/* <img src={`./images/${status.forecast}.jpg`} alt="forecast" /> */}
         <p>{status.day}</p>
         <ul>
           <li>{status.high} | {status.low}</li>
